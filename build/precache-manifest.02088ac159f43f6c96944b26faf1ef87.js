16c3e040e3ffd311a8c7efc5a1a1f5f2567e4b1f
miguelinera <miguelinera@hotmail.com>
2021-06-28 19:21:56 +0200

    initial commit

---
+++ build/precache-manifest.02088ac159f43f6c96944b26faf1ef87.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01453c555e9b32f69773cf2fd2689fec",
+    "url": "/keeper-app-test/index.html"
+  },
+  {
+    "revision": "5693703e7fe892af5b10",
+    "url": "/keeper-app-test/static/js/2.ff4ff6e6.chunk.js"
+  },
+  {
+    "revision": "85d997e2d507445fb3d6",
+    "url": "/keeper-app-test/static/js/main.21ff6491.chunk.js"
+  },
+  {
+    "revision": "774b292c7ee8f1976d67",
+    "url": "/keeper-app-test/static/js/runtime-main.54040fe7.js"
+  }
+]);
